cdd44e554912c8402cee776e72d0baca1fe5d874
github username <crishanks12@gmail.com>
2019-08-14 17:20:01 -0600

    added jwt to server

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,39 @@
 class ApplicationController < ActionController::API
+
+  # Auth
+  before_action :authorized
+
+  def encode_token(payload)
+    JWT.encode(payload, 's3cr3t')
+  end
+
+  def auth_header
+    request.headers['Authorization']
+  end
+
+  def decoded_token
+    if auth_header
+      token = auth_header.split(' ')[1]
+      begin
+        JWT.decode(toke, 's3cr3t', true)
+      rescue JWT::DecodeError
+        nil
+    end
+  end
+
+  def current_user
+    if decoded_token
+      user_id = decoded_token[0]['user_id']
+      @user = User.find_by(id: user_id)
+    end
+  end
+
+  def logged_in?
+    !!current_user
+  end
+
+  def authorized
+    render json: { message: 'Please log in' }, status: :unauthorized unless logged_in?
+  end
+
 end
